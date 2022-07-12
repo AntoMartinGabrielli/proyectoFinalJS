766e282108ebdbd10f6b7604d3c887cbbb661255
Anto.Martin <almartingab@gmail.com>
2022-07-11 23:25:28 -0300

    Correcciones

--- main.js
+++ main.js
@@ -2,39 +2,41 @@ let inputTemp = parseFloat(prompt("Numero a convertir"));
 
 let deTemp = prompt("Temperatura a convertir. Elegir entre C (celcius), F (Fahrenheit) y K (Kelvin).").toUpperCase();
 
+if ((deTemp != "C") && (deTemp !=  "F") && (deTemp != "K")){
+    alert("No es una medida existente. Por favor ingresar medida valida.");
+    deTemp = prompt(" Ingresar temperatura a convertir valida. Elegir entre C (celcius), F (Fahrenheit) y K (Kelvin).").toUpperCase();
+}
+
 let aTemp = prompt("Temperatura deseada. Elegir entre C (celcius), F (Fahrenheit) y K (Kelvin).").toUpperCase();
 
-
+if ((aTemp != "C") && (aTemp !=  "F") && (aTemp != "K")){
+    alert("No es una medida existente. Por favor ingresar medida valida.");
+    aTemp = prompt(" Ingresar temperatura deseada valida. Elegir entre C (celcius), F (Fahrenheit) y K (Kelvin).").toUpperCase();
+}
 
 function convertirTemp(){
-    const C = 30;
-    const F = 86;
-    const K = 303.15;
-    
-    let outputTemp = resultadoTemp;
-    
     /* celcius a fahrenheit */
-    if (deTemp == C && aTemp == F){
-        outputTemp = inputTemp*F/C;
+    if (deTemp == "C" && aTemp == "F"){
+        outputTemp = (inputTemp*1.8)+32;
     } /* celcius a kelvin */
-    else if (deTemp == C && aTemp == K){
-        outputTemp = inputTemp*K/C;
+    else if (deTemp == "C" && aTemp == "K"){
+        outputTemp = inputTemp + 273.15;
     } /* fahrenheit a celcius */
-    else if (deTemp == F && aTemp == C){
-        outputTemp = inputTemp*C/F;
+    else if (deTemp == "F" && aTemp == "C"){
+        outputTemp = (inputTemp-32)/1.8;
     }/* fahrenheit a kelvin */
-    else if (deTemp == F && aTemp == K){
-        outputTemp = inputTemp*K/F;
+    else if (deTemp == "F" && aTemp == "K"){
+        outputTemp = 5/9*(inputTemp-32)+273.15;
     } /* kelvin a celcius */
-    else if (deTemp == K && aTemp == C){
-        outputTemp = inputTemp*C/K;
+    else if (deTemp == "K" && aTemp == "C"){
+        outputTemp = inputTemp-273.15;
     }/* kelvin a fahrenheit */
-    else if (deTemp == K && aTemp == F){
-        outputTemp = inputTemp*F/K;
+    else if (deTemp == "K" && aTemp == "F"){
+        outputTemp = 1.8*(inputTemp-273.15)+32;
     } /* celcius a celcius, kelvin a kelvin, fahrenheit a fahrenheit */
     else {
         outputTemp = inputTemp;
     }
+    console.log(outputTemp);
 }
-console.log(outputTemp);
-
+convertirTemp()
