a9333925191ea9c8920c3a23b8bd140fc78e4744
Anto.Martin <almartingab@gmail.com>
2022-07-20 16:24:55 -0300

    1ra entrega proyecto

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
+class conversor{
+    constructor(medida){
+        this.medida = medida;
+    }
+}
+
+let listaMedidas = [{medida:"temperatura", unidades:"C, K y F"}, {medida:"volumen", unidades:"L, ML, CC y MC"}, {medida:"longitud", unidades:"MM, CM, M, KM, IN, FT y MI"}, {medida:"masa", unidades:"KG, G, OZ, LB y T"}]
+
+
+let busquedaMedidaUsuario = prompt("¿Qué medida desea convertir?").toLowerCase();
+let buscarMedida = listaMedidas.find(med => med.medida == busquedaMedidaUsuario);
+
+if (listaMedidas.some(med => med.medida == busquedaMedidaUsuario)){
+    alert(`La medida buscada es ${buscarMedida.medida} y sus unidades a disposicion para convertir son ${buscarMedida.unidades}.`)
+}else{
+    alert(`No poseemos esa medida para su conversion.`);
+    let agregarMedida = () => {
+        let medida = prompt("¿Que medida le gustaría que agregaramos en nuestra página?");
+    
+        let medidaNueva = new conversor (medida);
+        listaMedidas.push(medidaNueva);
+        console.log(listaMedidas)
+        alert(`Usted agrego ${medidaNueva.medida} a la lista de conversores de la pagina. Muchas gracias.`)
+    }
+    agregarMedida()
+}
+
+
 /* TEMPERATURA */
 
-let inputTemp = parseFloat(prompt("Numero a convertir"));
+let inputTemp = parseFloat(prompt("TEMPERATURA: Numero a convertir"));
 
 /* ACA INTENTE CON LA FORMA QUE ME DIJISTE Y DE OTRA FORMA QUE MOSTRO LA PROFE EN UNA DE LASC CLASES Y NO ME FUNCIONA */
 /* if(inputTemp.toString() == "NaN"){
@@ -25,22 +53,22 @@ if ((aTemp != "C") && (aTemp !=  "F") && (aTemp != "K")){
 function convertirTemp(){
     /* celcius a fahrenheit */
     if (deTemp == "C" && aTemp == "F"){
-        outputTemp = (inputTemp*1.8)+32;
+        outputTemp = Math.round((inputTemp*1.8)+32);
     } /* celcius a kelvin */
     else if (deTemp == "C" && aTemp == "K"){
-        outputTemp = inputTemp + 273.15;
+        outputTemp = Math.round(inputTemp + 273.15);
     } /* fahrenheit a celcius */
     else if (deTemp == "F" && aTemp == "C"){
-        outputTemp = (inputTemp-32)/1.8;
+        outputTemp = Math.round((inputTemp-32)/1.8);
     }/* fahrenheit a kelvin */
     else if (deTemp == "F" && aTemp == "K"){
-        outputTemp = 5/9*(inputTemp-32)+273.15;
+        outputTemp = Math.round(5/9*(inputTemp-32)+273.15);
     } /* kelvin a celcius */
     else if (deTemp == "K" && aTemp == "C"){
-        outputTemp = inputTemp-273.15;
+        outputTemp = Math.round(inputTemp-273.15);
     }/* kelvin a fahrenheit */
     else if (deTemp == "K" && aTemp == "F"){
-        outputTemp = 1.8*(inputTemp-273.15)+32;
+        outputTemp = Math.round(1.8*(inputTemp-273.15)+32);
     } /* celcius a celcius, kelvin a kelvin, fahrenheit a fahrenheit */
     else {
         outputTemp = inputTemp;
@@ -51,7 +79,7 @@ convertirTemp()
 
 /* VOLUMEN */
 
-let inputVol = parseFloat(prompt("Numero a convertir"));
+let inputVol = parseFloat(prompt("VOLUMEN: Numero a convertir"));
 
 let deVol = prompt("Volumen a convertir. Elegir entre L (Litro), ML (Mililitro), CC (Centimetro Cubico) y MC (Metro Cubico).").toUpperCase();
 
@@ -96,7 +124,7 @@ function convertirVol (){
         outputVol = inputVol*1000000;
     } /* L a L, ML a ML, CC a CC y MC a MC */
     else {
-        outerVol = inputVol;
+        outputVol = inputVol;
     } 
 
     console.log(outputVol);
@@ -104,46 +132,4 @@ function convertirVol (){
 convertirVol()
 
 
-class conversor{
-    constructor(medida){
-        this.medida = medida;
-    }
-}
 
-let listaMedidas = [{medida:"temperatura"}, {medida:"volumen"}, {medida:"longitud"}, {medida:"masa"}]
-
-let agregarMedida = () => {
-    let medida = prompt("¿Que medida le gustaría que agregaramos en nuestra página?");
-
-    let medidaNueva = new conversor (medida);
-    listaMedidas.push(medidaNueva);
-    console.log(listaMedidas)
-    alert(`Usted agrego ${medidaNueva} a la lista de conversores de la pagina. Muchas gracias.`)
-}
-
-agregarMedida()
-
-
-/* ESTO QUE ESTA COMENTADO ABAJO NO SE SI SERVIRA DE ALGO, INTENTE HACER PERO NO SE COMO CONECTARLO CON OTRAS COSAS, FUNCIONES, ETC */
-/* class conversor2{
-    constructor (temperatura, volumen, masa, longitud){
-        this.temperatura = temperatura;
-        this.volumen = volumen;
-        this.masa = masa;
-        this.longitud = longitud;
-    }
-}
-
-let medidas = [
-    {
-    temp1:"Celcius",
-    temp2:"Kelvin",
-    temp3:"Farenheit"
-    },
-    {
-    vol1:"Litro",
-    vol2:"Mililitro",
-    vol3:"Metro cubico",
-    vol4:"Centimetro Cubico"
-    }
-] */
