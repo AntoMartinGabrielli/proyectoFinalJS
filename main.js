3d93bef965147bc7c9250ac14cbed74182d195c3
Anto.Martin <almartingab@gmail.com>
2022-07-26 18:10:16 -0300

    Dom y eventos

--- main.js
+++ main.js
@@ -28,7 +28,8 @@ if (listaMedidas.some(med => med.medida == busquedaMedidaUsuario)){
 
 /* TEMPERATURA */
 
-let inputTemp = parseFloat(prompt("TEMPERATURA: Numero a convertir"));
+
+/* let inputTemp = parseFloat(prompt("TEMPERATURA: Numero a convertir")); */
 
 /* ACA INTENTE CON LA FORMA QUE ME DIJISTE Y DE OTRA FORMA QUE MOSTRO LA PROFE EN UNA DE LASC CLASES Y NO ME FUNCIONA */
 /* if(inputTemp.toString() == "NaN"){
@@ -36,7 +37,7 @@ let inputTemp = parseFloat(prompt("TEMPERATURA: Numero a convertir"));
     parseFloat(prompt("Por favor ingresar numero valido a convertir"));
 } */
 
-let deTemp = prompt("Temperatura a convertir. Elegir entre C (celcius), F (Fahrenheit) y K (Kelvin).").toUpperCase();
+/* let deTemp = prompt("Temperatura a convertir. Elegir entre C (celcius), F (Fahrenheit) y K (Kelvin).").toUpperCase();
 
 if ((deTemp != "C") && (deTemp !=  "F") && (deTemp != "K")){
     alert("No es una medida existente. Por favor ingresar medida valida.");
@@ -48,88 +49,118 @@ let aTemp = prompt("Temperatura deseada. Elegir entre C (celcius), F (Fahrenheit
 if ((aTemp != "C") && (aTemp !=  "F") && (aTemp != "K")){
     alert("No es una medida existente. Por favor ingresar medida valida.");
     aTemp = prompt(" Ingresar temperatura deseada valida. Elegir entre C (celcius), F (Fahrenheit) y K (Kelvin).").toUpperCase();
-}
+} */
+
+
+const inputTemp = document.getElementById('inputTemp');
+const outputTemp = document.getElementById('outputTemp');
+const deTemp = document.getElementById('deTemp');
+const aTemp = document.getElementById('aTemp');
+let deTempValue,aTempValue;
+
+inputTemp.addEventListener("keyup", convertirTemp);
+deTemp.addEventListener("change", convertirTemp);
+aTemp.addEventListener("change", convertirTemp);
+
+deTempValue = deTemp.value;
+aTempValue = aTemp.value;
+
 
 function convertirTemp(){
-    /* celcius a fahrenheit */
-    if (deTemp == "C" && aTemp == "F"){
-        outputTemp = Math.round((inputTemp*1.8)+32);
-    } /* celcius a kelvin */
-    else if (deTemp == "C" && aTemp == "K"){
-        outputTemp = Math.round(inputTemp + 273.15);
+    deTempValue = deTemp.value;
+    aTempValue = aTemp.value;
+
+    /* celcius a kelvin */
+    if (deTempValue == "C" && aTempValue == "K"){
+        outputTemp.value = Math.round(inputTemp.value+273.15);
+    }/* celcius a fahrenheit */
+    else if (deTempValue == "C" && aTempValue == "F"){
+        outputTemp.value = Math.round((inputTemp.value*1.8)+32);
     } /* fahrenheit a celcius */
-    else if (deTemp == "F" && aTemp == "C"){
-        outputTemp = Math.round((inputTemp-32)/1.8);
+    else if (deTempValue == "F" && aTempValue == "C"){
+        outputTemp.value = Math.round((inputTemp.value-32)/1.8);
     }/* fahrenheit a kelvin */
-    else if (deTemp == "F" && aTemp == "K"){
-        outputTemp = Math.round(5/9*(inputTemp-32)+273.15);
+    else if (deTempValue == "F" && aTempValue == "K"){
+        outputTemp.value = Math.round(5/9*(inputTemp.value-32)+273.15);
     } /* kelvin a celcius */
-    else if (deTemp == "K" && aTemp == "C"){
-        outputTemp = Math.round(inputTemp-273.15);
+    else if (deTempValue == "K" && aTempValue == "C"){
+        outputTemp.value = Math.round(inputTemp.value-273.15);
     }/* kelvin a fahrenheit */
-    else if (deTemp == "K" && aTemp == "F"){
-        outputTemp = Math.round(1.8*(inputTemp-273.15)+32);
+    else if (deTempValue == "K" && aTempValue == "F"){
+        outputTemp.value = Math.round(1.8*(inputTemp.value-273.15)+32);
     } /* celcius a celcius, kelvin a kelvin, fahrenheit a fahrenheit */
     else {
-        outputTemp = inputTemp;
+        outputTemp.value = inputTemp.value;
     }
-    console.log(outputTemp);
+    
 }
-convertirTemp()
+convertirTemp();
 
 /* VOLUMEN */
 
-let inputVol = parseFloat(prompt("VOLUMEN: Numero a convertir"));
+/* let inputVol = parseFloat(prompt("VOLUMEN: Numero a convertir"));
 
 let deVol = prompt("Volumen a convertir. Elegir entre L (Litro), ML (Mililitro), CC (Centimetro Cubico) y MC (Metro Cubico).").toUpperCase();
 
-let aVol = prompt("Volumen deseado. Elegir entre L (Litro), ML (Mililitro), CC (Centimetro Cubico) y MC (Metro Cubico).").toUpperCase();
+let aVol = prompt("Volumen deseado. Elegir entre L (Litro), ML (Mililitro), CC (Centimetro Cubico) y MC (Metro Cubico).").toUpperCase(); */
+
+const inputVol = document.getElementById('inputVol');
+const outputVol = document.getElementById('outputVol');
+const deVol = document.getElementById('deVol');
+const aVol = document.getElementById('aVol');
+let deVolValue,aVolValue;
+
+inputVol.addEventListener("keyup", convertirVol);
+deVol.addEventListener("change", convertirVol);
+aVol.addEventListener("change", convertirVol);
+
+deVolValue = deVol.value;
+aVolValue = aVol.value;
 
 function convertirVol (){
     /* litro a mililitro */
-    if (deVol == "L" && aVol == "ML"){
-        outputVol = inputVol*1000;
+    if (deVolValue == "L" && aVolValue == "ML"){
+        outputVol.value = inputVol.value*1000;
     } /* litro a centimetro cubico */
-    else if (deVol == "L" && aVol == "CC"){
-        outputVol = inputVol*1000;
+    else if (deVolValue == "L" && aVolValue == "CC"){
+        outputVol.value = inputVol.value*1000;
     } /* litro a metro cubico */
-    else if (deVol == "L" && aVol == "MC"){
-        outputVol = inputVol*0.001;
+    else if (deVolValue == "L" && aVolValue == "MC"){
+        outputVol.value = inputVol.value*0.001;
     } /* mililitro a litro */
-    else if (deVol == "ML" && aVol == "L"){
-        outputVol = inputVol/1000;
+    else if (deVolValue == "ML" && aVolValue == "L"){
+        outputVol.value = inputVol.value/1000;
     } /* mililitro a centimetro cubico */
-    else if (deVol == "ML" && aVol == "CC"){
-        outputVol = inputVol*1;
+    else if (deVolValue == "ML" && aVolValue == "CC"){
+        outputVol.value = inputVol.value*1;
     } /* mililitro a metro cubico */
-    else if (deVol == "ML" && aVol == "MC"){
-        outputVol = inputVol/1000000;
+    else if (deVolValue == "ML" && aVolValue == "MC"){
+        outputVol.value = inputVol.value/1000000;
     } /* centimetro cubico a litro */
-    else if (deVol == "CC" && aVol == "L"){
-        outputVol = inputVol/1000;
+    else if (deVolValue == "CC" && aVolValue == "L"){
+        outputVol.value = inputVol.value/1000;
     } /* centimetro cubico a mililitro */
-    else if (deVol == "CC" && aVol == "ML"){
-        outputVol = inputVol*1;
+    else if (deVolValue == "CC" && aVolValue == "ML"){
+        outputVol.value = inputVol.value*1;
     } /* centimetro cubico a metro cubico */
-    else if (deVol == "CC" && aVol == "MC"){
-        outputVol = inputVol/1000000;
+    else if (deVolValue == "CC" && aVolValue == "MC"){
+        outputVol.value = inputVol.value/1000000;
     } /* metro cubico a litro */
-    else if (deVol == "MC" && aVol == "L"){
-        outputVol = inputVol*1000;
+    else if (deVolValue == "MC" && aVolValue == "L"){
+        outputVol.value = inputVol.value*1000;
     } /* metro cubico a mililitro */
-    else if (deVol == "MC" && aVol == "ML"){
-        outputVol = inputVol*1000000;
+    else if (deVolValue == "MC" && aVolValue == "ML"){
+        outputVol.value = inputVol.value*1000000;
     } /* metro cubico a centimetro cubico */
-    else if (deVol == "MC" && aVol == "CC"){
-        outputVol = inputVol*1000000;
+    else if (deVolValue == "MC" && aVolValue == "CC"){
+        outputVol.value = inputVol.value*1000000;
     } /* L a L, ML a ML, CC a CC y MC a MC */
     else {
-        outputVol = inputVol;
+        outputVol.value = inputVol.value;
     } 
 
-    console.log(outputVol);
 }
-convertirVol()
+convertirVol();
 
 
 
